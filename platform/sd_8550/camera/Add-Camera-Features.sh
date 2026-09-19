dd769a21ad3926eb6b74255317d9e3fdc4910cab
mstivar01 <mstivr01@gmail.com>
2026-09-19 16:59:41 +0300

    Update Add-Camera-Features.sh

--- platform/sd_8550/camera/Add-Camera-Features.sh
+++ platform/sd_8550/camera/Add-Camera-Features.sh
@@ -1,19 +1,3 @@
-LOG_BEGIN "Adding 4k 120FPS On Pro Video..."
-
-CF "BACK_CAMCORDER_RESOLUTION_FEATURE_MAP_3840X2160_120FPS" \
-   "value=true" \
-   "hdr=true" \
-   "hdr10=false" \
-   "snapshot-support=false" \
-   "vdis=false" \
-   "super-vdis=false" \
-   "effect=false" \
-   "object-tracking=false" \
-   "seamless-zoom-support=false" \
-   "physical-zoom-supported=false" \
-   "external-storage-support=false" \
-   "supported-mode=pro_video,slow_motion"
-
 LOG_BEGIN "Adding Camera Assistant advanced features"
 
 CF "SUPPORT_CAMERA_ASSISTANT_ADAPTIVE_PIXEL" "value=true"
