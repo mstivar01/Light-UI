8a4701d1f356880750b94c7f25097102b21d5784
mstivar01 <mstivr01@gmail.com>
2026-09-19 16:24:01 +0300

    Remove MultiUser
    6.9 inc isnt that big is it?

--- arcadex/mods/multiuser/framework.jar/Enable-Multiuser-Tablet-Override.sh
+++ arcadex/mods/multiuser/framework.jar/Enable-Multiuser-Tablet-Override.sh
@@ -1,20 +1,4 @@
 
-DEVICE_CHARACTERISTICS="$(GET_PROP system ro.build.characteristics)"
-SINGLE_DEVICE_CHARACTERISTICS="${DEVICE_CHARACTERISTICS%%,*}"
+LOG_BEGIN "."
 
-SMALI_FILE="$(find . -name "MultiUserSupportsHelper.smali" -type f 2>/dev/null)"
-
-LOG_BEGIN "Enabling Multiuser patch..."
-
-# The bomb
-if ! sed -i "s/\"tablet\"/\"$SINGLE_DEVICE_CHARACTERISTICS\"/g" "$SMALI_FILE"; then
-    ERROR_EXIT "Failed to apply multiuser patch."
-fi
-
-# Set props
-BPROP "system" "persist.sys.show_multiuserui" "1"
-BPROP "system" "fw.max_users" "5"
-BPROP "system" "fw.show_multiuserui" "1"
-BPROP "system" "fw.showhiddenusers" "1"
-
-LOG_END "Multiuser patch applied"
+LOG_END "Nothing Done :D"
