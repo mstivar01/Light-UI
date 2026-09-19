b2ba5b06a45326044681b36580a5b9aebb907c32
mstivar01 <mstivr01@gmail.com>
2026-09-19 16:18:53 +0300

    Remove BSOH
    No need for something doesnt know how to work

--- arcadex/mods/bsoh/SecSettings.apk/Add-BSOH-Settings.sh
+++ arcadex/mods/bsoh/SecSettings.apk/Add-BSOH-Settings.sh
@@ -1,37 +1,4 @@
-# Scamsung only added this bomb in Galaxy A23
 
-BOMB_MODEL="SM-A236B"
-OLD_PROP="ro.product.model"
-NEW_PROP="ro.product.arcade.model"
+LOG_BEGIN "Doing nothing to BSOH Settings.."
 
-BPROP "system" "ro.product.arcade.model" "$STOCK_MODEL"
-
-LOG_BEGIN "Adding BSOH Settings.."
-
-# Add entries in floating feature
-
-FF "BATTERY_SUPPORT_BSOH_SETTINGS" "TRUE"
-FF "BATTERY_SUPPORT_SBP_INFO_SETTINGS" "TRUE"
-
-PLANT_MODEL="$STOCK_MODEL"
-
-find . -type f -name "*.smali" | while read -r smali; do
-    if grep -q "$BOMB_MODEL" "$smali"; then
-
-        # Replace bomb / plant
-        sed -i "s/$BOMB_MODEL/$PLANT_MODEL/g" "$smali"
-
-        sed -i "s/ro\.product\.model/$NEW_PROP/g" "$smali"
-
-    fi
-done
-
-# Real model name in settings
-find . -type f -name "ModelNameGetter.smali" | while read -r smali; do
-    if grep -q "ro.product.model" "$smali"; then
-        sed -i "s/ro\.product\.model/ro.boot.em.model/g" "$smali"
-
-    fi
-done
-
-LOG_END "BSOH patch applied"
+LOG_END "Nothing is done :D"
