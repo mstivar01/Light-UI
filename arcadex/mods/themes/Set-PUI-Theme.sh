b04eac11dbadda494dd4750a1f19978a0e3c94e1
mstivar01 <mstivr01@gmail.com>
2026-09-19 16:30:01 +0300

    Remove PIU theme
    What is that?

--- arcadex/mods/themes/Set-PUI-Theme.sh
+++ arcadex/mods/themes/Set-PUI-Theme.sh
@@ -1,14 +1,4 @@
-# ==============================================================================
-#
-# MOD_NAME="Add PUI theme for Galaxy"
-# MOD_AUTHOR="@FifthSnow and @HeheJuice"
-# MOD_DESC="Applies PUI theme overlays to system."
-#
-# ==============================================================================
 
-LOG_BEGIN "Adding PUI theme overlays.."
+LOG_BEGIN "Nothing"
 
-for f in "$SCRPATH/pui"/*.apk; do
-    [ -f "$f" ] || continue
-    cp -f "$f" "$WORKSPACE/product/overlay/"
 done
